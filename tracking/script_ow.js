453b9abc1ae27c7fb574b78400bbab7703094c34
Ethan Lane <ethantroublelane@gmail.com>
2025-04-01 22:32:08 -0400

    merge ow_tracker.js + script.js
    update ow_tracker.html to reflect source changes

--- tracking/script_ow.js
+++ tracking/script_ow.js
@@ -6,8 +6,39 @@
                                                                                                                                             was able to restore from a small edit made with github copliot a couple days ago.
 
 */
+/* ####################################################################### */
+/* combine script.js */
+/* developed by Ethan Lane 
+    started: 3/21/25
+*/
+function getPlayersFromSession() {
+    const playerNames = JSON.parse(sessionStorage.getItem('playerNames') || '[]'); // Ensure valid JSON
+    const players = {};
+    playerNames.forEach(name => {
+        const playerData = sessionStorage.getItem(name);
+        if (playerData && playerData !== "undefined") { // Check for valid JSON
+            players[name] = JSON.parse(playerData);
+        }
+    });
+    return players;
+}
 
+function getPlayerFromSession(selectedPlayer) {
+    return JSON.parse(sessionStorage.getItem(selectedPlayer));
+}
 
+function addPlayerToSession(playerName, playerData) {
+    const playerNames = JSON.parse(sessionStorage.getItem('playerNames')) || [];
+    if (!playerNames.includes(playerName)) {
+        playerNames.push(playerName);
+        sessionStorage.setItem('playerNames', JSON.stringify(playerNames));
+    }
+    sessionStorage.setItem(playerName, JSON.stringify(playerData));
+}
+
+function getHeroesFromSession() {
+    return JSON.parse(sessionStorage.getItem('heroes'));
+}
 /* ####################################################################### */
 
 /* Gobal Variables */
