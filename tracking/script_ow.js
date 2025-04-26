363c038b4bee0ca8de62a0906f442ced5649981b
Ethan Lane <ethantroublelane@gmail.com>
2025-04-25 22:42:24 -0400

    stable version of fall back Interface
    updated the ow_tracker.html to reflect changes in script_ow.js,
    
    added new interface for when the API the data normally is gotten from is either down or not working properly.

--- tracking/script_ow.js
+++ tracking/script_ow.js
@@ -1,7 +1,8 @@
 /*  Developed by Ethan Lane
     Stated: 3/17/25
     sources: uses the publicly avaible, overfast api
-                                                                    
+    If the api is down the code goes to "fall back mode" and a speacial interface has users manually enter data
+*/                                                             
 /* ################################# Session Storage ####################################### */
 /**
  * Retrieves a list of players stored in sessionStorage.
@@ -57,8 +58,10 @@ function getHeroesFromSession() {
  * Fetches hero stats and player summaries if not already stored.
  */
 async function ASYNC_main() { // these variables/functions need to use the await keyword
+    if (sessionStorage.getItem('isInFallBackMode')){console.log('fallback moode');setFallBackUI();return;} // check if in fall back mode after page reloads
     if (!hero_stats){hero_stats = await getHeroes();console.log('fetching hero names');}
     if (!player_summary){player_summary = await getPlayerStatsSummary(selectedPlayer);console.log('fetching player summary');}
+    if (fallback) {return;} // stop when player bad
     await populateDropdown('heroDropdown');
     await populateStatDropdown('statDropdown');
 
@@ -82,7 +85,15 @@ async function getPlayerStatsSummary(BattleTag) {
         const data = await response.json();
         return data;
     } catch (error) {
-        console.error(error, 'not ok');
+        if (BattleTag !== default_profile) {
+            console.error(error, 'not ok');
+            return;
+        }
+
+        // If the default profile fails than we likely cannt rely on the API to get anything a fallback system takes over for complete manual input of data
+        fallback = true;
+        sessionStorage.setItem('isInFallBackMode', true);
+        setFallBackUI();
     }
 }
 
@@ -122,7 +133,7 @@ function iterateStorage(jsonData) {
  */
 function displayStats() {
     const heroName = document.getElementById("heroDropdown").value;
-    const statsContainer = document.getElementById("stats-container");
+    const statsinfo = document.getElementById("stats-container");
     statsData = player_summary;
     if (heroName && statsData.heroes[heroName]) {
         const stats = statsData.heroes[heroName];
@@ -130,9 +141,9 @@ function displayStats() {
         for (const [key, value] of Object.entries(stats)) {
             statsHtml += `<p><strong>${key}:</strong> ${value}</p>`;
         }
-        statsContainer.innerHTML = statsHtml;
+        statsinfo.innerHTML = statsHtml;
     } else {
-        statsContainer.innerHTML = `<h2>Stats</h2><p>Select a hero to see their stats.</p>`;
+        statsinfo.innerHTML = `<h2>Stats</h2><p>Select a hero to see their stats.</p>`;
     }
 }
 
@@ -274,18 +285,20 @@ async function getHeroStats(heroName) {
  * @param {Object} data - The data to save.
  */
 function saveData(data) {
-    document.getElementById('write').innerText = JSON.stringify(data, null, 2);
+    document.getElementById('output').innerText = JSON.stringify(data, null, 2);
 }
 
 /**
  * Populates datalist on text input with BattleTags from stored in session storage.
  */
 function populateBattleTagList() {
-    JSON.parse(sessionStorage.getItem('playerNames')).forEach(name => {
-        const option = document.createElement('option');
-        option.value = name;
-        document.getElementById('battleTagList').appendChild(option);
-    });
+    if (sessionStorage.getItem('playerNames')) { // dont let empty session storage cause problems
+        JSON.parse(sessionStorage.getItem('playerNames')).forEach(name => {
+            const option = document.createElement('option');
+            option.value = name;
+            document.getElementById('battleTagList').appendChild(option);
+        });
+    }
 }
 
 /* ################################# Chart 2 ####################################### */
@@ -435,42 +448,240 @@ function updateHeroStatChart() { /* call is nested inside call to other chart to
  */
 async function addPlayerData() {
     const battleTagInput = document.getElementById('battleTagInput').value;
-    if (battleTagInput != selectedPlayer) {
-        selectedPlayer = battleTagInput;
-        if (selectedPlayer in getPlayersFromSession()) {
-            player_summary = getPlayerFromSession(selectedPlayer);
-            console.log('Variables Update FROM SESSION FORMAT\nname:', selectedPlayer, 'summary:', player_summary);
-        } else {
-            player_summary = await getPlayerStatsSummary(selectedPlayer);
-            if (!player_summary) {
-                alert('Player not found or does not exists\nThis could be due to the account being private or not existing\nIf you believe this is an error this input will not be accepted until your session storage is clear (close your browser and try again)');
-                console.log('player summary is null, data stored to prevent future requests');
-                return; // NULL value supplied to data, stops from breaking the program
+    if (!sessionStorage.getItem('isInFallBackMode')) {
+        if (battleTagInput != selectedPlayer) {
+            selectedPlayer = battleTagInput;
+            if (selectedPlayer in getPlayersFromSession()) {
+                player_summary = getPlayerFromSession(selectedPlayer);
+                console.log('Variables Update FROM SESSION FORMAT\nname:', selectedPlayer, 'summary:', player_summary);
+            } else {
+                player_summary = await getPlayerStatsSummary(selectedPlayer);
+                if (!player_summary) {
+                    alert('Player not found or does not exists\nThis could be due to the account being private or not existing\nIf you believe this is an error this input will not be accepted until your session storage is clear (close your browser and try again)');
+                    console.log('player summary is null, data stored to prevent future requests');
+                    return; // NULL value supplied to data, stops from breaking the program
+                }
+                addPlayerToSession(selectedPlayer, player_summary);
+                console.log('Variables Update FROM FETCH\nname:', selectedPlayer, 'summary:', player_summary);
             }
-            addPlayerToSession(selectedPlayer, player_summary);
-            console.log('Variables Update FROM FETCH\nname:', selectedPlayer, 'summary:', player_summary);
+            document.getElementById('battletag').innerText = `BattleTag: ${selectedPlayer}`; // Update the BattleTag display
         }
-        document.getElementById('battletag').innerText = `BattleTag: ${selectedPlayer}`; // Update the BattleTag display
-    }
-    if (!player_summary) {
-        alert('Further input of this username is not accepted to reduce invalid requests\nIf you believe this is an error this input will not be accepted until your session storage is clear (close your browser and try again)');
-        console.log('forbidden battletag');
-        return; // NULL value supplied to data, stops from breaking the program
+        if (!player_summary) {
+            alert('Further input of this username is not accepted to reduce invalid requests\nIf you believe this is an error this input will not be accepted until your session storage is clear (close your browser and try again)');
+            console.log('forbidden battletag');
+            return; // NULL value supplied to data, stops from breaking the program
+        }
+    } else {
+        console.log('updating from fallback mode');
     }
     updateChart();
     updateStatChart();
     updateHeroStatChart();
 }
 
-/* ############################### MAIN ######################################## */
+/* ############################### Variables ######################################## */
 
 /* Gobal Variables */
 //var selectedPlayer = 'NightTrain-11944'; old default profile, is not found since new update to game
-var selectedPlayer = 'TeKrop-2217'
+
+// 'https://ow-api.com/v1/stats/pc/us/TeKrop-2217/profile' new api
+const default_profile = 'TeKrop-2217'
+var selectedPlayer = default_profile;
 var hero_stats = getHeroesFromSession();
 var player_summary = getPlayerFromSession(selectedPlayer);
 var mode = null;
+var fallback = false;
+
+/* ############################### FallBack System ######################################## */
+
+
+let players = {};
+/**
+ * Manages formatting user input from the fallback UI to be used by the rest of the logic
+ */
+function addStat() {
+    const nameInput = document.getElementById('playerName');
+    const statInput = document.getElementById('statSelect');
+    const valueInput = document.getElementById('statValue');
+    const heroInput = document.getElementById('heroName');
+
+    const playerName = nameInput.value.trim();
+    const stat = statInput.value;
+    const value = Number(valueInput.value);
+    const hero = heroInput.value;
+    
+    // new player
+    if (!players[playerName]) {
+        players[playerName] = {
+            "general": {
+                "games_lost": 0,
+                "games_played": 0,
+                "games_won": 0,
+                "kda": 0,
+                "time_played": 0,
+                "winrate": 0
+                },
+            "heroes":{},
+            "roles":{}
+        }
+    }
+
+    // new hero
+    if (!players[playerName].heroes[hero]) {
+        players[playerName].heroes[hero] = {};
+    }
+
+    // update hero values
+    players[playerName].heroes[hero][stat] = value;
+    players[playerName].heroes[hero].name = hero;
+    
+    // Display updated JSON and update player summary
+    player_summary = players[playerName];
+    addPlayerToSession(selectedPlayer, JSON.stringify(players, null, 2)); // updates player summary in session storage
+    document.getElementById('output').textContent = JSON.stringify(players, null, 2);
+    
+
+    // update dropdown menus
+    hero_stats = player_summary.heroes;
+    let temp = [];
+    for (const [key, value] of Object.entries(hero_stats)) {
+        temp.push({ key: key, name: key }); // combine key and name into one object
+    }
+    hero_stats = temp;
+
+    // adds heroes to session storage since that step is bypassed in the mode this function gets called
+    sessionStorage.setItem('heroes', JSON.stringify(hero_stats));
+
+    // set up dropdown boxes for loading manual data
+    document.getElementById('heroDropdown').innerHTML = '<option value="">-- Select a Hero --</option>';
+    document.getElementById('statDropdown').innerHTML = '<option value="">-- Select a Stat --</option>';
+    populateDropdown('heroDropdown');
+    populateStatDropdown('statDropdown');
+}
+
+/**
+ * Creates a UI for manual data input.
+ * To be used when API collection of data fails.
+ */
+function setFallBackUI() {
+    // graph settings
+    selectedPlayer = "Offline-Mode";
+    players = JSON.parse(getPlayerFromSession('Offline-Mode'));
+    if (!players){players={};} // if that last line returns null this fixes it
+
+    const info = document.getElementById("info");
+
+    // delete and modify existing UI
+    document.getElementById('battletag').innerHTML = 'Offline Mode';
+    document.getElementById('battleTagInput').outerHTML = ``;
+    document.getElementById('heroLabel').style.display = '';
+    document.getElementById('statLabel').style.display = '';
+    //document.getElementById('addData').outerHTML = '';
+    document.getElementById('')
+
+    // change styling to work with new UI                        
+    info.style.columnCount = 1; // changes styling to work for fallback system  
+
+
+    // creates input for player name
+    const nameLabel = document.createElement("label");
+    nameLabel.textContent = "Player Name:";
+    const nameInput = document.createElement("input");
+    nameInput.type = "text";
+    nameInput.text = "Default";
+    nameInput.id = "playerName";
+    nameInput.placeholder = "Enter Player Name";
+
+    // Adds a hero name input
+    const heroLabel = document.createElement("label");
+    heroLabel.textContent = "Hero Name:";
+    const heroName = document.createElement("input");
+    heroName.type = "text";
+    heroName.id = 'heroName';
+    heroName.placeholder = 'Enter Hero Name';
+
+    // creates a dropdrop of stats
+    const statLabel = document.createElement("label");
+    statLabel.textContent = "Stat:";
+    const statSelect = document.createElement("select");
+    statSelect.id = "statSelect";
+
+    const stats = ["games_lost", "games_played", "games_won", "kda", "time_played", "winrate"];
+    stats.forEach(stat => {
+        const option = document.createElement("option");
+        option.value = stat;
+        option.textContent = stat;
+        statSelect.appendChild(option);
+    });
+
+    // creates feilds to input values
+    const valueLabel = document.createElement("label");
+    valueLabel.textContent = "Value:";
+    const valueInput = document.createElement("input");
+    valueInput.type = "number";
+    valueInput.id = "statValue";
+    valueInput.placeholder = "Enter Stat Amount";
+
+
+    // Adds a button to add stats
+    const button = document.createElement("button");
+    button.textContent = "Add Stat";
+    button.onclick = addStat;
+
+    // create explanation for constructed data section
+    const explain = document.createElement("h2");
+    explain.innerHTML = "The Data Below Can Be Output From The Interface Above Or Manually Written Below";
+
+    // create a element to output user input convert to JSON data into
+    const output = document.createElement("pre");
+    output.id = "output";
+    output.contentEditable = true;
+    output.spellcheck = false;
+
+    const directions = document.createElement("h2")
+    directions.innerHTML = "Entering information below with unlock and add values to the selection boxes above."
+    
+    // append into html
+    info.appendChild(directions)
+    info.appendChild(nameLabel);
+    info.appendChild(nameInput);
+    info.appendChild(document.createElement("br"));
+    info.appendChild(heroLabel);
+    info.appendChild(heroName);
+
+    info.appendChild(statLabel);
+    info.appendChild(statSelect);
+    info.appendChild(document.createElement("br"));
+
+    info.appendChild(valueLabel);
+    info.appendChild(valueInput);
+    info.appendChild(document.createElement("br"));
+    info.appendChild(document.createElement("br"));
+
+    info.appendChild(button);
+    info.appendChild(explain);
+    info.appendChild(document.createElement("h3")).textContent = "Currently Constructed Data:";
+    info.appendChild(output);
+    addStat();
+    document.getElementById(output.id).addEventListener('keydown', setPlayerToOutput);
+    document.getElementById(output.id).addEventListener('keyup', setPlayerToOutput);
+}
+
+function setPlayerToOutput() {
+    output = document.getElementById('output');
+    try {
+        players = JSON.parse(output.textContent);
+        addPlayerToSession('Offline Mode', JSON.stringify(players), null, 2);
+        output.style.backgroundColor = 'rgb(62, 192, 73)';
+    } catch (error) {
+        output.style.backgroundColor = '#c20f0f';
+    }
+}
+
+/* ############################### MAIN ######################################## */
 
 /* Main */
+document.getElementById('heroLabel').style.display = 'none';
+document.getElementById('statLabel').style.display = 'none';
 ASYNC_main();
 document.addEventListener('DOMContentLoaded', populateBattleTagList());
